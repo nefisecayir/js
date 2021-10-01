557ba84eee8d203bbea35ed01698ab902e5c0ed7
Nefise Çayır <pinkuugg@gmail.com>
2021-10-01 13:16:05 +0300

    if_else-app.js

---
+++ if_else-app.js
@@ -0,0 +1,46 @@
+// IF-ELSE APP
+
+//  var  firstRegistration = new Date ('07/09/2019')
+//  firstRegistration.setHours(0,0,0,0);
+//  var intrafficMs = Date.now() - firstRegistration.getTime();
+//  var intrafficDay = Math.floor(intrafficMs/(1000*60*60*24));
+ 
+
+//  if(intrafficDay<=365){
+//      console.log('first car care service time');
+     
+//  }else if(intrafficDay > 365 && intrafficDay <= 365*2){
+//      console.log('second car care service time');
+//  }else if(intrafficDay > 365*2 && intrafficDay <= 365*3){
+//      console.log('third car care service time');
+//  }else{
+//      console.log('an unkown time');
+//  }
+
+//      console.log(intrafficDay);
+
+var result = prompt("who is it?");
+
+
+
+if(result == 'cancel'){
+    console.log('cancelled');
+}else if(result == 'nefise'){
+    console.log('welcome~');
+
+    var password = prompt('enter your password:');
+
+    if(password == 'cancel'){
+        console.log('cancelled');
+
+    }else if (password == '1234'){
+        console.log('hi there!')
+    }else{
+        console.log('write the correct password.')
+    }
+}else{
+    console.log('i dont know you!');
+}
+
+
+
