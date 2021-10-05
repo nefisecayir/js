6e4d4558c3abd88e087414beafa059cb26005160
Nefise Çayır <pinkuugg@gmail.com>
2021-10-05 11:03:56 +0300

    for loop added at the end

--- loops-app.js
+++ loops-app.js
@@ -13,6 +13,7 @@ var number = Math.floor((Math.random()*10)+1);
 bullet = Number(prompt('in how many tries you are gonna find the right number?'))
 life = bullet;
 
+
 console.log(number);
         // *1 ile 9 arası random sayılar verir.*
 
@@ -34,3 +35,29 @@ console.log(number);
                 console.log('no more life left, GAME OVER. the number was : '+number);
             }
         }
+
+        
+// **for (loop)*
+
+// for(let i=0; i<10; i++){
+//     console.log(i);
+// }
+    // *0'dan 9'a kadar sayıları sıralayıp konsola yazar*
+
+
+// for(let i=0; i<=10;  i++){
+//     if(i==9){
+//         console.log('my fav number: ' +i);
+//         continue;
+                // *continue dendiği zaman aynı rakam bir daha yazılmaz, kod 4'den devam eder.*
+    
+//     }
+    
+//     if(i==6){
+//         console.log('the number i dont like is: 6');
+//         break;
+                 // *break dendiği zaman o rakam sonrası yazılmaz, kod 6'dan sonra yazılmaya devam etmez. (işletilmez)*
+//     }
+// console.log(i)
+    
+// }
