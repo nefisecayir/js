3cd40067363cbf3a8c974a3fd0c3595a14d716e7
Nefise Çayır <pinkuugg@gmail.com>
2021-10-09 09:06:31 +0300

    array task added.

---
+++ js-self/array-task.js
@@ -0,0 +1,11 @@
+const array = [0,1,2,3,4,5,6,7,8,9];
+
+function arr(num){
+    for (let num=0; num<10; num++){
+        console.log(num);
+        
+    }
+}
+arr()
+
+console.log(`this arrays length is : ${array.length}`)
