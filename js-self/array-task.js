b469c3b6b239443ce80e1f53dd429589cf010970
Nefise Çayır <pinkuugg@gmail.com>
2021-10-09 22:51:44 +0300

    map function added.

--- js-self/array-task.js
+++ js-self/array-task.js
@@ -1,11 +1,21 @@
 const array = [0,1,2,3,4,5,6,7,8,9];
 
-function arr(num){
-    for (let num=0; num<10; num++){
-        console.log(num);
+// function arr(){
+//     for (let num=0; num<10; num++){
+//         console.log(num);
         
-    }
-}
-arr()
+//     }
+// }
+// arr()
+
+// console.log(`this arrays length is : ${array.length}`)
+
+
+
+
+array.map(function(arr){
+    arr;
+    console.log(arr);
+});
 
 console.log(`this arrays length is : ${array.length}`)
